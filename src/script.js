0b02c929651dddd95ef10630133a0c19bc5e9639
Biraj Dahal <bdahal@gatech.edu>
2024-03-27 22:34:50 -0400

    new version

--- src/script.js
+++ src/script.js
@@ -1,114 +1,13 @@
-import Graph from "graphology";
-import Sigma from "sigma";
-import subgraph from 'graphology-operators/subgraph';
-import { connectedComponents } from 'graphology-components';
-
 import peri from "./peri.json"
-import nodeinfo from "./nodeinfo.json"
 
-import { oneName, transformData, zoomToPos } from "./utils.js"
+import { oneName, transformData, zoomToPos, renderNetworks, set_components_from_ai } from "./utils.js"
 
 import './style.css';
 
-function set_components_from_ai(ai) {
-  let networks = []
-
-  let nodes = []
-  let edges = {}
-
-  // populate objects
-  let keys = Object.keys(ai)
-  for (let i = 0; i < keys.length; i++) {
-    let key = keys[i]
-    let alist = ai[key].slice().sort()
-
-    if (alist.length == 1) {
-      nodes.push(alist[0])
-    }
-    else {
-      for (let j = 0; j < alist.length; j++) {
-        for (let k = j + 1; k < alist.length; k++) {
-          let edge = [alist[j], alist[k]]
-          edge.sort()
-          let ename = `${edge[0]}-${edge[1]}`
-
-          nodes.push(alist[j])
-          nodes.push(alist[k])
-
-          if (edges[ename]) {
-            edges[ename] += 1 / (alist.length - 1)
-          }
-          else {
-            edges[ename] = 1 / (alist.length - 1)
-          }
-        }
-      }
-    }
-  }
-
-  // construct graph
-  let gdata = {}
-  nodes = new Set(nodes)
-  gdata.nodes = []
-  gdata.edges = []
-
-  for (const edge in edges) {
-    let newedge = {}
-    newedge.key = edge
-    newedge.source = edge.slice(0, edge.indexOf("-"))
-    newedge.target = edge.slice(edge.indexOf("-") + 1)
-    newedge.attributes = {"weight": edges[edge], "distance": 1 / edges[edge], "size": 1, "color": "#aaaaaa"}
-
-    gdata.edges.push(newedge)
-  }
-
-  nodes = Array.from(nodes)
-  for (let i = 0; i < nodes.length; i++) {
-    let node = nodes[i]
-    let newnode = {}
-    newnode.key = node
-    gdata.nodes.push(newnode)
-  }
-
-  let graph = Graph.from(gdata)
-  let nameToId = {}
-  graph.forEachNode( node => {
-    graph.setNodeAttribute(node, 'label', oneName(nodeinfo[node][0]).trim())
-    graph.setNodeAttribute(node, 'fullname', nodeinfo[node][0])
-    graph.setNodeAttribute(node, 'x', nodeinfo[node][2])
-    graph.setNodeAttribute(node, 'y', nodeinfo[node][3])
-  } );
-
-  let components = connectedComponents(graph);
-  components.sort((a, b) => b.length - a.length);
-  for (let i = 0; i < components.length; i++) {
-    let gc = subgraph(graph, components[i])
-    networks.push(gc)
-  }
-
-  if (components.length == 0) {
-    networks = [ graph ];
-  }
-
-  let numsize = {}
-  let str = ""
-  for (let i = 0; i < networks.length; i++) {
-    let size = networks[i].nodes().length
-
-    if (numsize[size]) {
-      numsize[size] += 1;
-      str += `<option value=${i}>${networks[i].nodes().length}${String.fromCharCode(64 + numsize[size])} Nodes</option>`
-    }
-    else { 
-      numsize[size] = 1;
-      str += `<option value=${i}>${networks[i].nodes().length} Nodes</option>`
-    }
-  }
-
-  $("#component-select").get(0).innerHTML = str
-
-  window.components = networks
-  window.nameToId = nameToId
+function searchScientist(input) {
+  let name = input.value
+  input.value = ""
+  console.log(name)
 }
 
 function updateYears(table) {
@@ -156,21 +55,8 @@ function updateComponent() {
   renderNetworks()
 }
 
-function renderNetworks() {
-  if (window.renderer) { 
-    window.renderer.kill()
-  }
-
-  window.renderer = new Sigma(window.components[window.component], $("#network").get(0), {
-    labelRenderedSizeThreshold: 1, 
-    labelDensity: 0.5,
-    defaultNodeColor: "#00662B",
-  })
-  window.renderer.camera.maxRatio = 1
-  window.renderer.refresh();
-}
-
 function authorCell(cellname, params, onRendered) {
+  //console.log(cellname.getValue(), cellname["_cell"].row.position)
   let names = cellname.getValue().split("; ")
   let cell = document.createElement("div")
   cell.className = "author-cell"
@@ -189,8 +75,6 @@ function authorCell(cellname, params, onRendered) {
     for (let j = 0; j < authors.length; j += 1)
       authors[j].addEventListener("click", () => {
         let node = window.pa_list["ai"][cellname["_cell"].row.position - 1][j]
-        console.log(node)
-        console.log(authors[j].getValue("node"))
         let nattr = window.renderer.nodeDataCache[node]
         zoomToPos(nattr.x, nattr.y)
       })
@@ -203,6 +87,8 @@ $(document).ready(function() {
   window.pa_list = JSON.parse(peri)
   let transposed = transformData(window.pa_list)
 
+  console.log(transposed)
+
   set_components_from_ai(window.pa_list["ai"])
 
   window.component = 0
@@ -212,10 +98,9 @@ $(document).ready(function() {
     data: transposed,
     layout: "fitData",
     columns: [
-      { title: "Authors", field: "af", width: "22%", formatter: authorCell },
-      { title: "Title", field: "ti", width: "50%" },
-      { title: "Year", field: "py",  width: "13%" },
-      { title: "Source", field: "so", width: "14%" },
+      { title: "Authors", field: "af", width: "35%", formatter: authorCell,  resizable: false, variableHeight:true},
+      { title: "Title", field: "ti", width: "52%", resizable: false, formatter:"textarea"},
+      { title: "Year", field: "py",  width: "10%", resizable: false},
     ]
   })
 
@@ -223,5 +108,10 @@ $(document).ready(function() {
     $("#minyear").get(0).addEventListener("change", () => updateYears(table));
     $("#maxyear").get(0).addEventListener("change", () => updateYears(table));
     $("#component-select").get(0).addEventListener("change", () => updateComponent());
+    $("#nodesearch").get(0).addEventListener("keyup", (e) => {
+      if (e.key == "Enter" || e.keyCode == 13) {
+        searchScientist(e.target);
+      }
+    });
   });
 })
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,10 @@
+import Sigma from "sigma";
+import subgraph from 'graphology-operators/subgraph';
+import Graph from "graphology";
+import { connectedComponents } from 'graphology-components';
+
+import nodeinfo from "./nodeinfo.json"
+
 export function oneName(name) {
   return name.split(";").sort((a, b) => b.length - a.length)[0]
 }
@@ -12,7 +19,10 @@ export function transformData(data) {
       keys.forEach(key => {
           row[key] = data[key][i];
       });
-      transformed.push(row);
+
+      if (row[keys[0]]) {
+        transformed.push(row);
+      }
   }
 
   return transformed;
@@ -22,4 +32,119 @@ export function zoomToPos(x, y) {
   window.renderer.camera.x = x
   window.renderer.camera.y = y
   window.renderer.refresh()
+}
+
+export function renderNetworks() {
+  if (window.renderer) { 
+    window.renderer.kill()
+  }
+
+  window.renderer = new Sigma(window.components[window.component], $("#network").get(0), {
+    labelRenderedSizeThreshold: 1, 
+    labelDensity: 0,
+    defaultNodeColor: "#00662B",
+  })
+  window.renderer.camera.maxRatio = 1
+  window.renderer.refresh();
+}
+
+export function set_components_from_ai(ai) {
+  window.nodeinfo = nodeinfo
+  let networks = []
+
+  let nodes = []
+  let edges = {}
+
+  // populate objects
+  let keys = Object.keys(ai)
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    let alist = ai[key].slice().sort()
+
+    if (alist.length == 1) {
+      nodes.push(alist[0])
+    }
+    else {
+      for (let j = 0; j < alist.length; j++) {
+        for (let k = j + 1; k < alist.length; k++) {
+          let edge = [alist[j], alist[k]]
+          edge.sort()
+          let ename = `${edge[0]}-${edge[1]}`
+
+          nodes.push(alist[j])
+          nodes.push(alist[k])
+
+          if (edges[ename]) {
+            edges[ename] += 1 / (alist.length - 1)
+          }
+          else {
+            edges[ename] = 1 / (alist.length - 1)
+          }
+        }
+      }
+    }
+  }
+
+  // construct graph
+  let gdata = {}
+  nodes = new Set(nodes)
+  gdata.nodes = []
+  gdata.edges = []
+
+  for (const edge in edges) {
+    let newedge = {}
+    newedge.key = edge
+    newedge.source = edge.slice(0, edge.indexOf("-"))
+    newedge.target = edge.slice(edge.indexOf("-") + 1)
+    newedge.attributes = {"weight": edges[edge], "distance": 1 / edges[edge], "size": 1, "color": "#aaaaaa"}
+
+    gdata.edges.push(newedge)
+  }
+
+  nodes = Array.from(nodes)
+  for (let i = 0; i < nodes.length; i++) {
+    let node = nodes[i]
+    let newnode = {}
+    newnode.key = node
+    gdata.nodes.push(newnode)
+  }
+
+  let graph = Graph.from(gdata)
+  graph.forEachNode( node => {
+    graph.setNodeAttribute(node, 'label', oneName(nodeinfo[node][0]).trim())
+    graph.setNodeAttribute(node, 'fullname', nodeinfo[node][0])
+    graph.setNodeAttribute(node, 'x', nodeinfo[node][2])
+    graph.setNodeAttribute(node, 'y', nodeinfo[node][3])
+    graph.setNodeAttribute(node, 'size', 3)
+  } );
+
+  let components = connectedComponents(graph);
+  components.sort((a, b) => b.length - a.length);
+  for (let i = 0; i < components.length; i++) {
+    let gc = subgraph(graph, components[i])
+    networks.push(gc)
+  }
+
+  if (components.length == 0) {
+    networks = [ graph ];
+  }
+
+  let numsize = {}
+  let str = ""
+  for (let i = 0; i < networks.length; i++) {
+    let size = networks[i].nodes().length
+
+    if (numsize[size]) {
+      numsize[size] += 1;
+      str += `<option value=${i}>${networks[i].nodes().length}${String.fromCharCode(64 + numsize[size])} Nodes</option>`
+    }
+    else { 
+      numsize[size] = 1;
+      str += `<option value=${i}>${networks[i].nodes().length} Nodes</option>`
+    }
+  }
+
+  $("#component-select").get(0).innerHTML = str
+
+  window.components = networks
 }
